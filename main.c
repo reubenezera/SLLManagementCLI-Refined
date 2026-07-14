ecc6a7f53bb0a6ad6bc9df9d925df910b526075e
reubenezera <reuben.e.agoes@gmail.com>
2026-07-14 21:42:39 +0700

    Comment out head and tail debug print statements
    Commented out debug print statements for head and tail.

--- main.c
+++ main.c
@@ -85,8 +85,8 @@ void showList(struct linkedList *L) {
 		showNodes(L);
 	}
 	//Head & Tail Checkers for Debugging
-	printf("Head: %d\n", L->head->data);
-	printf("Tail: %d\n", L->tail->data);
+	/*printf("Head: %d\n", L->head->data);
+	printf("Tail: %d\n", L->tail->data);*/
 }
 
 //Insertion @ Head
@@ -274,7 +274,7 @@ int main() {
 		}
 
 	} while (choice != 6);
-  
+	
 	printf("\nSession Ended\n");
 	showList(L);
 	nodeCount(L);
